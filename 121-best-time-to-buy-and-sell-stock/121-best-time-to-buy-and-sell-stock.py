bba21bc2e9ba8b68f01ef545c037ac376f2ef814
heechang_jeong <63540952+gmlckd1213@users.noreply.github.com>
2022-07-05 19:09:08 +0900

    Time: 2252 ms (6.69%), Space: 25.2 MB (6.58%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,22 @@
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         dp = [0] * len(prices)
+#         cheapest = prices[0]
+#         for i in range(1, len(prices)):
+#             cheapest = min(cheapest, prices[i])
+#             if prices[i] > cheapest:
+#                 dp[i] = max(dp[i - 1], prices[i] - cheapest)
+#             else:
+#                 dp[i] = dp[i - 1]
+
+#         return dp[-1]
+    
+    
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp = [0] * len(prices)
-        cheapest = prices[0]
-        for i in range(1, len(prices)):
-            cheapest = min(cheapest, prices[i])
-            if prices[i] > cheapest:
-                dp[i] = max(dp[i - 1], prices[i] - cheapest)
-            else:
-                dp[i] = dp[i - 1]
-
-        return dp[-1]
+        price_min = 10000
+        profit = 0
+        for price_current in prices:
+            price_min = min(price_current, price_min)
+            profit = max(profit, price_current-price_min)
+        return profit
