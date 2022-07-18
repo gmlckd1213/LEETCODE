4aa154e7f71818816a19c8c8ae307a7782b30b67
heechang_jeong <63540952+gmlckd1213@users.noreply.github.com>
2022-07-18 15:56:14 +0900

    Time: 36 ms (82.71%), Space: 13.9 MB (10.56%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.py
+++ 771-jewels-and-stones/771-jewels-and-stones.py
@@ -1,8 +1,8 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        a=list(jewels)
-        su = 0
-        for s in a:
-            su += stones.count(s)
-        return su
+        je=list(jewels)
+        sum = 0
+        for s in je:
+            sum += stones.count(s)
+        return sum
         
