d4dbd2f4faedc18781ae7f43183a4eea171eef56
heechang_jeong <63540952+gmlckd1213@users.noreply.github.com>
2022-07-18 15:55:37 +0900

    Time: 41 ms (69.58%), Space: 13.9 MB (10.56%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.py
+++ 771-jewels-and-stones/771-jewels-and-stones.py
@@ -3,7 +3,6 @@ class Solution:
         a=list(jewels)
         su = 0
         for s in a:
-            n=stones.count(s)
-            su += n
+            su += stones.count(s)
         return su
         
