7364a1972db476670b5115652e3c92283330f769
heechang_jeong <63540952+gmlckd1213@users.noreply.github.com>
2022-07-18 16:00:47 +0900

    Time: 54 ms (32.61%), Space: 14 MB (10.56%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.py
+++ 771-jewels-and-stones/771-jewels-and-stones.py
@@ -1,14 +1,18 @@
 class Solution:
-    # def numJewelsInStones(self, jewels: str, stones: str) -> int:
-    #     je=list(jewels)
-    #     sum = 0
-    #     for s in je:
-    #         sum += stones.count(s)
-    #     return sum
-      def numJewelsInStones(self, jewels: str, stones: str) -> int:
-            freqs = collections.Counter(stones)
-            count = 0
-            for char in jewels:
-                count += freqs[char]
-            return count
-        
+
+#     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+#         je=list(jewels)
+#         sum = 0
+#         for s in je:
+#             sum += stones.count(s)
+#         return sum
+    
+#     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+#         freqs = collections.Counter(stones)
+#         count = 0
+#         for char in jewels:
+#             count += freqs[char]
+#         return count
+    
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        return sum(s in jewels for s in stones)
