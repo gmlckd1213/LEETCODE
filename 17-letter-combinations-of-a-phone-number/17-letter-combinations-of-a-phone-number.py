cd2e54caced56139637e70a4599fb7f68a919ef6
heechang_jeong <63540952+gmlckd1213@users.noreply.github.com>
2022-07-22 18:56:49 +0900

    Time: 64 ms (11.17%), Space: 13.8 MB (98.79%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,23 +1,24 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        dial = {'2':['a','b','c'], '3':['d','e','f'], '4':['g','h','i'], '5':['j','k','l'], '6':['m','n','o'],'7':['p','q','r','s'], '8':['t','u','v'],'9':['w','x','y','z']}
-        if len(digits) == 0:
-            return
-        elif len(digits) ==1:
-            return dial[digits]
-        else:
-            result = []
-            for i in digits:
-                result.append(dial[i])
-            if len(result)==2:
-                a=product(result[0],result[1])
-            elif len(result)==3:
-                a=product(result[0],result[1],result[2])
-            elif len(result)==4:
-                a=product(result[0],result[1],result[2],result[3])
-                
-            
-            t=[]
-            for i in a:
-                t.append("".join(i))
-        return t
+        def dfs(index, path):
+            # 끝까지 탐색하면 백트래킹
+            if len(path) == len(digits):
+                result.append(path)
+                return
+
+            # 입력값 자릿수 단위 반복
+            for i in range(index, len(digits)):
+                # 숫자에 해당하는 모든 문자열 반복
+                for j in dic[digits[i]]:
+                    dfs(i + 1, path + j)
+
+        # 예외 처리
+        if not digits:
+            return []
+
+        dic = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+               "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+        result = []
+        dfs(0, "")
+
+        return result
