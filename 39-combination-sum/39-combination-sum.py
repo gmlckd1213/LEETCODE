c6e4b324c4e536b6e6fa822dfab1e2e3bb2f7139
heechang_jeong <63540952+gmlckd1213@users.noreply.github.com>
2022-07-22 21:12:47 +0900

    Time: 5285 ms (5.00%), Space: 14.1 MB (58.66%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -3,13 +3,13 @@ class Solution:
         min_num = min(candidates)
         max_num = max(candidates)
 
-        start = math.ceil(target / max_num)
-        end = math.ceil(target / min_num)
+        start = int(target / max_num)
+        end = int(target / min_num)
         result = []
 
         for index in range(start, end + 1):
             for comb in itertools.combinations_with_replacement(candidates, index):
                 if sum(comb) == target:
-                    result.append(list(comb))
+                    result.append((comb))
 
         return result
