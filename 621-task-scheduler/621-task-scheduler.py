122f4cc0df4ec9564f006ee9bf200837f4407b35
heechang_jeong <63540952+gmlckd1213@users.noreply.github.com>
2022-08-10 19:10:27 +0900

    Time: 1880 ms (5.01%), Space: 14.2 MB (90.18%) - LeetHub

---
+++ 621-task-scheduler/621-task-scheduler.py
@@ -0,0 +1,17 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        counter = collections.Counter(tasks)
+        result = 0
+        while True:
+            sub_count = 0
+            for task, _ in counter.most_common(n+1):
+                sub_count += 1
+                result += 1
+                counter.subtract(task)
+                # 0 이하인 아이템 목록에서 제거
+                counter += collections.Counter()# 빈 collection.counter()를 더하는 것인데 0이하인 아이템을 목록에서 제거
+            if not counter:
+                break
+            result += n - sub_count + 1
+        
+        return result
