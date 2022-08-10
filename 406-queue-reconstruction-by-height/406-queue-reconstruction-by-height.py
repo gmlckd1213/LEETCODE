160ff8bdad30505e7ff2f9e5ae95b04026a80fc8
heechang_jeong <63540952+gmlckd1213@users.noreply.github.com>
2022-08-10 17:28:39 +0900

    Time: 128 ms (76.24%), Space: 14.6 MB (31.07%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.py
@@ -1,19 +1,19 @@
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        # people.sort(key=lambda p: (-p[0], p[1]))
-        # res = []
-        # for p in people:
-        #     res.insert(p[1], p)
-        # return res
+        people.sort(key=lambda p: (-p[0], p[1]))
+        res = []
+        for p in people:
+            res.insert(p[1], p)
+        return res
         
-        heap = []
-        for person in people:
-            heapq.heappush(heap, (-person[0],person[1]))#최대힙활용 - 
+#         heap = []
+#         for person in people:
+#             heapq.heappush(heap, (-person[0],person[1]))#최대힙활용 - 
             
-        result = []
-        while heap:
-            person = heapq.heappop(heap)
-            result.insert(person[1], [-person[0], person[1]])
-        return result
+#         result = []
+#         while heap:
+#             person = heapq.heappop(heap)
+#             result.insert(person[1], [-person[0], person[1]])
+#         return result
     
-        #시간복잡도 O(n+log(n))
+#         #시간복잡도 O(n+log(n))
