358ac80c53c841e16231c1a9011ea99abe542bd4
heechang_jeong <63540952+gmlckd1213@users.noreply.github.com>
2022-08-10 16:07:21 +0900

    Time: 107 ms (41.00%), Space: 15.2 MB (69.01%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,8 +1,13 @@
+
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+
         result = 0
         for i in range(len(prices)-1):
             if prices[i+1] > prices[i]:
                 result += prices[i+1] - prices[i]
+
         return result
         
+# 시간복잡도 O(n)
