3fa222d5068430a8fcbe1d70e0e7e77a351bc1d7
heechang_jeong <63540952+gmlckd1213@users.noreply.github.com>
2022-08-17 19:31:26 +0900

    Time: 82 ms (41.88%), Space: 19.9 MB (5.02%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -4,10 +4,8 @@ class Solution:
         for i in s:
             if i.isalnum():
                 l.append(i.lower())
-               
-        # while len(l) > 1:
-        #     if l.pop(0) != l.pop():
-        #         return False
-        # return True
         return l == l[::-1]
+
+ 
+
         
